3886efa1c0f52e60811a40afa4a3e181cc1a1193
LindaPermata <permatalinda531@gmail.com>
2024-06-03 14:22:49 +0700

    membuat fungsi main dengan string baris, untuk membuka file dan osstream outfile untuk menunjuk ke  sebuah nama file dan outfile open untuk membuka file

--- iOFile.cpp
+++ iOFile.cpp
@@ -6,5 +6,10 @@ using namespace std;
 int main() {
     string baris;
 
-    // membuka
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 }
