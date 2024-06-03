0416557735b754c9a40cc65b2a5c3540f3070f25
LindaPermata <permatalinda531@gmail.com>
2024-06-03 14:28:12 +0700

    membuat outfile.close untuk menutup file

--- iOFile.cpp
+++ iOFile.cpp
@@ -23,4 +23,6 @@ int main() {
         //menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+    // selesai dalam menulis file sekarang tutup filenya
+    outfile.close();
 }
