a80f39b8626844ded48d9a1cf1190c5203868432
LindaPermata <permatalinda531@gmail.com>
2024-06-03 14:44:44 +0700

    membuat ofstream outfile dan outfile open untuk membuka file dan menunjuk nama file

--- iOFileWithPath.cpp
+++ iOFileWithPath.cpp
@@ -10,6 +10,13 @@ int main() {
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
 
-    
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+
 
 }
