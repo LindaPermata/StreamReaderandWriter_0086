dfdfe667f6a4fb0814bc5935d2cdfcd99ff4b2ab
LindaPermata <permatalinda531@gmail.com>
2024-06-03 15:03:49 +0700

    membuat program manual exception

--- ManualException.cpp
+++ ManualException.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
-    
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "Pengeculian akan dieksekusi" << endl;
+    }
+    catch (...) {
+        /*jika selain integer maka nlock ini akan dieksekusi*/
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    return 0;
 }
